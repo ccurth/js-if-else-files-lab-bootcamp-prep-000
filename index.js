d687543d7110784b1f6123ee73e358ba88a67236
thoughtful-calculator-1463 <ccurth@gmail.com>
2018-10-15 22:32:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var name = "Joe"
 var height = "74"
 
+console.log("Name:")
+console.log(name)
+ 
+console.log("Height:")
+console.log(height)
+
 // Don't worry about this
 module.exports = { name, height
 }
